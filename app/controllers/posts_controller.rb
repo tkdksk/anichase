2afbc4e69c6a5f563e03ce4471afbcee7b5c189b
tkdksk <dakata0112@outlook.jp>
2020-07-02 14:51:34 +0000

    20200702

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    @newpost = Post.new(:reply_id => params[:id],
+    @newpost = Post.new(reply_id: params[:id],
       member_id: current_member&.id, name: current_member&.name)
     @posts = Post.where(reply_id: params[:id])
   end
 
   def create
     @post = Post.new(post_params)
-    @post.save 
+    @post.save
     redirect_to anime_path(params[:post][:anime_id]) if @post.reply_id.blank?
     redirect_to post_path(params[:post][:reply_id]) if @post.anime_id.blank?
   end
